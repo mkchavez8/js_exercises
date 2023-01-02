f945288a9c38f0db4b38c80a779cb9ee4cb99a72
Mossimo Chavez <114546057+mkchavez8@users.noreply.github.com>
2023-01-02 15:34:33 -0500

    Edit event listener and add disableButton function
    Edited the results element to only show once per click. Additionally
    added a disableButton function that stops the game at 5.

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 let playerScore = 0;
 let computerScore = 0;
 
-// Take player choice and callback playRound function
 const buttons = document.querySelectorAll('button')
+
+function disableButtons() {
+    buttons.forEach(button => {
+        button.disabled = true
+    })
+}
+
+// Place each event listener (click) into the playRound function as playerSelection
 buttons.forEach(button => {
     button.addEventListener('click', function () {
         playRound(button.className);
@@ -30,7 +37,6 @@ function playRound(playerSelection) {
 
     let computerSelection = getComputerChoice();
     let result = "";
-    const container = document.querySelector('.container');
 
     if (playerSelection === computerSelection) {
         result = 'Draw';
@@ -43,7 +49,8 @@ function playRound(playerSelection) {
         computerScore += 1;
             
             if (computerScore == 5) {
-                result = 'You lost the match';
+                result = 'You lost the match, refresh to play again!';
+                disableButtons();
             }
     }
     else if (
@@ -55,16 +62,11 @@ function playRound(playerSelection) {
         playerScore += 1;
 
             if (playerScore == 5) {
-                result = 'You won the match';
+                result = 'You won the match, refresh to play again!';
+                disableButtons();
             }
     }
-    else if (playerSelection !== 'Rock' || 'Paper' || 'Scissors') {
-        result = 'Invalid input';
-    }
-    
-    const returnResult = document.createElement('div');
-    returnResult.textContent = result + ' ' + playerScore + ' - ' + computerScore;
-    container.appendChild(returnResult);
-    return;
 
+    let returnResult = result + ' ' + playerScore + ' - ' + computerScore;
+    document.getElementById('results').textContent = returnResult;
 }
